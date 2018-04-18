9218f0c95e863867ce68f6784b43d4a16ad5187c
Marco-PC\Marco <marco.fong.hk@gmail.com>
2018-04-18 13:49:30 +0800

    iframe Test

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import time
 import subprocess
 import sys
 from cmd import Cmd
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
 # example.py
+driver = webdriver.Chrome()
+driver.get("http://bet.hkjc.com/")
+driver.implicitly_wait(5)
+if(driver.find_element_by_id('iframeDisplay')):
+     print("hi")
