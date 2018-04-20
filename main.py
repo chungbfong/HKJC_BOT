207d45280ab38226527dc1b10cbe177bc88d9692
Marco Fong <marco.fong.hk@gmail.com>
2018-04-20 12:49:08 +0800

    MuituBets Feature Implemented

--- main.py
+++ main.py
@@ -8,13 +8,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
-
+driver = webdriver.Chrome()
 # example.py
 
-start = time.time()
 
 username = 'ccfongaa3'
-password = 'pwwwwww'
+password = 'pwwww'
 
 q1 = '你求學時最喜愛的科目?'
 q2= '你兒時住在哪區?'
@@ -29,41 +28,45 @@ type = '_HAD_H_c'
 matchid = '125953'
 bet = '10'
 
+loginFlag= ''
 
-def buy(type,matchid,bet):
-    if (driver.find_element_by_id('iframeDisplay')):
+
+def buy(type,matchid,bet,remainingBets):
+    print(loginFlag)
+    if(loginFlag == False):
         login()
 
+    driver.switch_to.parent_frame()
+    driver.switch_to.frame(driver.find_element_by_id('info'))
+
+    driver.find_element_by_xpath(str('//*[@id="rmid' + matchid +'"]/td[1]/span/a')).click()
+
+    # matchid_HAD_(H/D/A)_c                  eg: 125904_HAD_H_c/ 125904_HAD_D_c/
+    # matchid_HDC_(H/A)_c                    eg: 125904_HDC_A_c/ 125904_HDC_H_c
+    try:
         driver.switch_to.parent_frame()
-        driver.switch_to.frame(driver.find_element_by_id('info'))
+        myElem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'info')))
 
-        driver.find_element_by_xpath(str('//*[@id="rmid' + matchid +'"]/td[1]/span/a')).click()
+    except TimeoutException:
+        print("Loading took too much time!")
+    driver.switch_to.frame(driver.find_element_by_id('info'))
+    temp = matchid + type
 
-        # matchid_HAD_(H/D/A)_c                  eg: 125904_HAD_H_c/ 125904_HAD_D_c/
-        # matchid_HDC_(H/A)_c                    eg: 125904_HDC_A_c/ 125904_HDC_H_c
-        try:
-            driver.switch_to.parent_frame()
-            myElem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'info')))
+    try:
 
-        except TimeoutException:
-            print("Loading took too much time!")
-        driver.switch_to.frame(driver.find_element_by_id('info'))
-        temp = matchid + type
+        myElem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, temp )))
 
-        try:
+    except TimeoutException:
+        print("Loading took too much time!")
+    driver.find_element_by_id(temp).click()
 
-            myElem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, temp )))
+    driver.find_element_by_xpath('//*[@id="oHeader_ALL"]/table/tbody/tr/td[4]/a/img').click()
 
-        except TimeoutException:
-            print("Loading took too much time!")
-        driver.find_element_by_id(temp).click()
+    print(matchid, " ", type," ", bet+"HKD")
 
-        driver.find_element_by_xpath('//*[@id="oHeader_ALL"]/table/tbody/tr/td[4]/a/img').click()
-
-        print(matchid, " ", type," ", bet+"HKD")
-
-        # submit bets
-        driver.switch_to.parent_frame()
+    # submit bets
+    driver.switch_to.parent_frame()
+    try:
         driver.switch_to.frame(driver.find_element_by_id('betSlipFrame'))
         driver.find_element_by_xpath('//*[@id="inputAmount0"]').click()
         driver.find_element_by_xpath('//*[@id="inputAmount0"]').clear()
@@ -71,67 +74,72 @@ def buy(type,matchid,bet):
         driver.find_element_by_xpath('//*[@id="pic_preview"]').click()
         driver.implicitly_wait(2)
         driver.switch_to.frame(driver.find_element_by_id('previewFrame'))
+        if(remainingBets != 0):
+            driver.find_element_by_xpath('//*[@id="pic_back"]').click()
 
         '''
         driver.find_element_by_xpath('//*[@id="pic_send_bet"]').click()
-
+    
         driver.implicitly_wait(2)
-
+    
         if(driver.find_elements_by_xpath(str("//*[contains(text(), 'BET REJECTED : 1')]"))):
             print('Bet Rejected!')
             if (driver.find_elements_by_xpath(str("//*[contains(text(), 'INSUFFICIENT BALANCE')]"))):
                 print('INSUFFICIENT BALANCE')
-
+    
         else:
             print('Bet Accepted!')
-
+    
         '''
+    except:
+        print("Betting system close")
+    time.sleep(3)
+    driver.switch_to.parent_frame()
+    driver.switch_to.parent_frame()
+    driver.switch_to.frame(driver.find_element_by_id('info'))
+    driver.find_element_by_xpath('//*[@id="topMenu"]/table/tbody/tr[2]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td[2]/a/img').click()
 
-        time.sleep(10)
-        driver.close()
+    time.sleep(3)
 
-    end = time.time()
-
-    elapsed = end - start
-
-    print(elapsed - 10)
 
 
 
 def login():
-    if (driver.find_element_by_id('iframeDisplay')):
-        driver.switch_to.frame(driver.find_element_by_id('betSlipFrame'))
-        driver.find_element_by_id('account').click()
-        driver.find_element_by_id('account').send_keys(username)
-        driver.implicitly_wait(3)
-        driver.find_element_by_id('passwordInput1').click()
-        driver.find_element_by_id('password').send_keys(password)
-        driver.find_element_by_id('pic_login').click()
-        if (driver.find_elements_by_xpath(str("//*[contains(text(), '" + q1 + "')]"))):
-            driver.find_element_by_id('ekbaDivInput').send_keys(q1ans)
 
-        elif (driver.find_elements_by_xpath(str("//*[contains(text(), '" + q2 + "')]"))):
-            driver.find_element_by_id('ekbaDivInput').send_keys(q2ans)
+    driver.switch_to.frame(driver.find_element_by_id('betSlipFrame'))
+    driver.find_element_by_id('account').click()
+    driver.find_element_by_id('account').send_keys(username)
+    driver.implicitly_wait(3)
+    driver.find_element_by_id('passwordInput1').click()
+    driver.find_element_by_id('password').send_keys(password)
+    driver.find_element_by_id('pic_login').click()
+    if (driver.find_elements_by_xpath(str("//*[contains(text(), '" + q1 + "')]"))):
+        driver.find_element_by_id('ekbaDivInput').send_keys(q1ans)
 
-        else:
-            driver.find_element_by_id('ekbaDivInput').send_keys(q3ans)
-        driver.find_element_by_id('pic_confirm').click()
-        driver.find_element_by_id('btn_enter').click()
+    elif (driver.find_elements_by_xpath(str("//*[contains(text(), '" + q2 + "')]"))):
+        driver.find_element_by_id('ekbaDivInput').send_keys(q2ans)
 
-        print('Login Successful')
-        print(driver.find_element_by_id('valueAccName').text)
-        print(driver.find_element_by_id('valueAccBal').text)
+    else:
+        driver.find_element_by_id('ekbaDivInput').send_keys(q3ans)
+    driver.find_element_by_id('pic_confirm').click()
+    driver.find_element_by_id('btn_enter').click()
 
-
-
-driver = webdriver.Chrome()
-driver.get("http://bet.hkjc.com/football/default.aspx?lang=en")
-driver.implicitly_wait(2)
-buy(type,matchid,bet)
+    print('Login Successful')
+    print(driver.find_element_by_id('valueAccName').text)
+    print(driver.find_element_by_id('valueAccBal').text)
+    loginFlag = True
 
 
+def main():
+    driver.get("http://bet.hkjc.com/football/default.aspx?lang=en")
+    driver.implicitly_wait(1)
+    login()
+    buy(type,matchid,bet,1)
+    buy(type, '125954',bet,0)
+    driver.close()
 
 
+main()
 
 
 
